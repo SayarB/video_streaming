a3151623a8a0173a90b3c903b1ef0c312cea6b8b
SayarB <sayarbhattacharyya03@gmail.com>
2021-10-13 19:39:06 +0530

    added some functionality

--- index.js
+++ index.js
@@ -1,49 +1,41 @@
-const express = require('express');
-const app=express();
-const fs = require('fs');
-const moviePath= {
-    rayep1:"RayMovie/Episode1(Forget me not).mkv",
-    rayep2:"RayMovie/Episode2(Bahrupia).mkv",
-    rayep3:"./RayMovie/Episode3(Hungama hai kyon bar).mkv",
-    rayep4:"./RayMovie/Episode4(Spotlight).mkv"
-}
-
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const movies = require("./moviePath").moviePath;
 require("dotenv").config();
 app.use(express.static("public"));
-app.listen(3000, ()=>
-{
-    console.log("listening on port 3000");
-})
-app.get('/', (req,res)=>
-{
-    console.log(req.hostname)
-    res.sendFile(__dirname+"/index.html");
-})
-app.get('/video', (req,res)=>
-{
-    const range = req.headers.range;
-    if (!range){
-        res.status(400).send("Requires Range Header");
-    }
-    
-    const videoPath = moviePath.rayep4;
-    const videoSize = fs.statSync(videoPath).size;
-
-    const chunk_size= 10 ** 6;
-    const start = Number (range.replace(/\D/g, ""));
-    const end = Math.min(start+chunk_size, videoSize-1);
-
-    const contentLength =  end-start+1;
-    const headers={
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges":"bytes",
-        "Content-Length": contentLength,
-        "Content-Type": "video/mkv",
-    }
-
-    res.writeHead(206, headers);
-
-    const videoStream = fs.createReadStream(videoPath, {start,end});
-    videoStream.pipe(res);
+app.listen(8000, () => {
+  console.log("listening on port 8000");
 });
+app.get("/", (req, res) => {
+  console.log(req.hostname);
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/video", (req, res) => {
+  const range = req.headers.range;
+  if (!range) {
+    res.status(400).send("Requires Range Header");
+  }
+  const videoObj = movies.conjuring3;
+  const videoPath = videoObj.path;
+  const format = videoObj.format;
+  const subs = videoObj.subtitleFile;
+  const videoSize = fs.statSync(videoPath).size;
 
+  const chunk_size = 10 ** 6;
+  const start = Number(range.replace(/\D/g, ""));
+  const end = Math.min(start + chunk_size, videoSize - 1);
+
+  const contentLength = end - start + 1;
+  const headers = {
+    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+    "Accept-Ranges": "bytes",
+    "Content-Length": contentLength,
+    "Content-Type": "video/" + format,
+  };
+
+  res.writeHead(206, headers);
+
+  const videoStream = fs.createReadStream(videoPath, { start, end });
+  videoStream.pipe(res);
+});
--- moviePath.js
+++ moviePath.js
@@ -1,7 +1,16 @@
-export const moviePath= {
-    "rayep1":"RayMovie/Episode1(Forget me not).mkv",
-    "rayep2":"RayMovie/Episode2(Bahrupia).mkv",
-    "rayep3":"RayMovie/Episode3(Hungama hai kyon bar)",
-    "rayep4":"RayMovie/Episode4(Spotlight)"
-    ""
-}
+module.exports = {
+  moviePath: {
+    money_heist_s1e1: { path: "", format: "" },
+    bariwali: { path: "../bariwali/bariwali.mkv", format: "mkv" },
+    f9: { path: "../f9/f9.mkv", format: "mkv" },
+    now_you_see_me: {
+      path: "../now_you_see_me/now_you_see_me.mp4",
+      format: "mp4",
+    },
+    conjuring3: {
+      path: "../conjuring3/movie.mp4",
+      format: "mp4",
+      subtitleFile: "../conjuring3/subs_en.srt",
+    },
+  },
+};
