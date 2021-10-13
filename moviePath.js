957a57651e2cb226682b7156ad87f72c2dfb36c0
SayarB <sayarbhattacharyya03@gmail.com>
2021-10-13 18:53:55 +0530

    created streaming server

---
+++ moviePath.js
@@ -0,0 +1,7 @@
+export const moviePath= {
+    "rayep1":"RayMovie/Episode1(Forget me not).mkv",
+    "rayep2":"RayMovie/Episode2(Bahrupia).mkv",
+    "rayep3":"RayMovie/Episode3(Hungama hai kyon bar)",
+    "rayep4":"RayMovie/Episode4(Spotlight)"
+    ""
+}
