1fd8b8817c16000726fd1492ce62603f2b5e2bc0
SayarB <sayarbhattacharyya03@gmail.com>
2021-11-24 13:54:17 +0530

    changes made

--- moviePath.js
+++ moviePath.js
@@ -12,5 +12,21 @@ module.exports = {
       format: "mp4",
       subtitleFile: "../conjuring3/subs_en.srt",
     },
+    james_bond: {
+      path: "../no_time_to_die/no_time_to_die.mp4",
+      format: "mp4",
+    },
+    a_quiet_place: {
+      path: "../a_quiet_place/a_quiet_place.mp4",
+      format: "mp4",
+    },
+    a_quiet_place_2: {
+      path: "../a_quiet_place_2/a_quiet_place_2.mp4",
+      format: "mp4",
+    },
+    dhamaka: {
+      path: "../dhamaka/dhamaka.mkv",
+      format: "mkv",
+    },
   },
 };
